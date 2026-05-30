a26af70930c9c9e89184fa52dbe7d463e1149ef6
Shreya Paladhi <146852844+Shreya-2803@users.noreply.github.com>
2026-05-30 15:55:05 +0530

    Time: 4 ms (9.44%) | Memory: 19.8 MB (5.25%) - LeetSync

---
+++ 91-decode-ways/decode-ways.py
@@ -0,0 +1,14 @@
+from functools import cache
+class Solution:
+    def numDecodings(self, s: str) -> int:
+        @cache
+        def dfs(i):
+            if i==len(s):
+                return 1
+            if s[i]=='0':
+                return 0
+            ways=dfs(i+1)
+            if (i+1<len(s) and 10<=int(s[i:i+2])<=26):
+                ways+=dfs(i+2)
+            return ways
+        return dfs(0)
