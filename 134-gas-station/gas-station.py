ba780907dbf34c1ad3becd40ec99ee7833cb569e
Shreya Paladhi <146852844+Shreya-2803@users.noreply.github.com>
2026-08-13 15:34:23 +0530

    Time: 33 ms (27.16%) | Memory: 26.1 MB (30.20%) - LeetSync

---
+++ 134-gas-station/gas-station.py
@@ -0,0 +1,15 @@
+class Solution:
+    def canCompleteCircuit(self, gas: List[int], cost: List[int]) -> int:
+        total=0
+        tank=0
+        start=0
+        for i in range(len(gas)):
+            balance=gas[i]-cost[i]
+            total+=balance
+            tank+=balance
+            if tank<0:
+                start=i+1
+                tank=0
+        if total<0:
+            return -1
+        return start
